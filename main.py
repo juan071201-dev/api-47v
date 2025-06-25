91887e9e6ed0f67cedb89e06417f688f64105e1a
juan071201-dev <jamh_07_12_01@hotmail.com>
2025-06-24 21:59:00 -0500

    Ingresado

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template
+import sqlite3
+
+def get_db_connection():
+    conn = sqlite3.connect("database.db")
+    conn.row_factory = sqlite3.Row
+    return conn
 
 app = Flask(__name__)
 
@@ -8,7 +14,15 @@ def base():
 
 @app.route('/home', methods=['GET'])
 def home():
-    return render_template('home.html')
+    titulo = "esta es el html home como variable"
+    return render_template('home.html', titulo=titulo)
+
+@app.route('/posts', methods=['GET'])
+def get_all_posts():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('post/post_list.html', posts=posts)
 
 if __name__ == '__main__':
     app.run(debug=True)
