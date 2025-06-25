979b9d87c6e556d47af7026377cae533ef94f430
juan071201-dev <jamh_07_12_01@hotmail.com>
2025-06-24 21:25:16 -0500

    readme for database

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-x = 5
-y = 8
-z = 20
-q = 90
-w = 50
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def base():
+    return render_template('base.html')
+
+@app.route('/home', methods=['GET'])
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
